ece2f6286c43c11ae9f763d769bc295fee596f0d
Antti Turunen <asturune@hotmail.com>
2021-09-23 10:32:08 +0300

    basicit react typescriptiin liittyen lisätty -tehtävä 9.14

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+//Headerin renderöintiä varten
+const Header = ({ name }: { name: string }) => (
+  <h1>{name}</h1>
+);
+
+//CourseParts -oliota varten tyyppi
+interface ContentProps {
+  name: string,
+  exerciseCount: number
+
+}
+
+//Arrayta varten tyyppi
+interface ContentPropsArray {
+  courses: Array<ContentProps>
+}
+
+
+//Contentin renderöinti. Tuodaan "App" -komponentilta data arrayna ja siksi tarvitaan
+//toinen interface Array:ta varten.
+//HUOM! Vaatii "<div>" -elementin return:ssa
+const Content = ({ courses }: ContentPropsArray): JSX.Element => {
+  console.log('COURSES', courses)
+  {
+    return (
+      <div>
+        {courses.map((course, index) => (<p key={index}>{course.name} {course.exerciseCount}</p>))}
+      </div>);
+  }
+
+}
+//Kurssin harjoitusten kokonaismäärän renderöinti
+//hyödynnetään "ContentPropsArray" -interfacea
+const Total = ({ courses }: ContentPropsArray): JSX.Element => {
+  {
+    return (
+      <div>
+        Number of exercises{" "}
+        {courses.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </div>);
+  }
+};
+
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header name={courseName} />
+      <Content courses={courseParts} />
+      <Total courses={courseParts} />
     </div>
   );
-}
+};
 
 export default App;
